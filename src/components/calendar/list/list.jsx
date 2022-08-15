ee8facf23a6674faacbba6b5eadbdcfa6449a718
ejaman <leegm042@gmail.com>
2022-08-15 15:58:27 +0900

    refector: styles 파일 모두 삭제

--- src/components/calendar/list/list.jsx
+++ src/components/calendar/list/list.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import styles from "./list.module.css";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import interactionPlugin from "@fullcalendar/interaction"; //
 import listPlugin from "@fullcalendar/list";
 import { ScheduleForm } from "../../Component/Container";
-import { useEffect } from "react/cjs/react.production.min";
 
 const List = ({ schedules, onAdd }) => {
   const obj = Object.assign({}, schedules);
